60aa9eacc963a445bb0fba7ea6d032e34f5b7d09
Kei Ishikawa <keiqueue@gmail.com>
2024-10-09 19:58:26 -0400

    title changed

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student Information System</h1>
-      <h5>Developed By: 101426567 || Kei Ishikawa || DevOps</h5>
+      <h5>Developed By: 101426567 || Kei Ishikawa || DevOps -COMP3104</h5>
       <StudentForm addStudent={addStudent} />
       <StudentList students={students} />
     </div>
